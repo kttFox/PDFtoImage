35b6733f1c77aab54a85262ee680e32b345b53ad
kttFox <kttFoxOnFire@gmail.com>
2022-10-12 13:49:30 +0900

    作成

---
+++ Source/PDFtoImage/MainWindow.xaml.cs
@@ -0,0 +1,289 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Windows.Data.Pdf;
+using Windows.Storage.Streams;
+using Libraries;
+
+namespace PDFtoImage {
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+
+			progressBar.Visibility = Visibility.Hidden;
+			comboBoxOldValue = comboBoxDPI.Text;
+		}
+
+		private async void Window_ContentRendered( object sender, EventArgs e ) {
+
+			var target = VisualTreeHelpers.FindVisualChild<TextBox>( comboBoxDPI );
+			target.LostFocus += this.ComboBoxDPI_LostFocus;
+
+
+			var args = Environment.GetCommandLineArgs();
+
+			if( args.Length <= 1 ) {
+				return;
+			}
+
+			await ドロップ( args );
+		}
+
+		private async void Button_Click( object sender, RoutedEventArgs e ) {
+
+			var initialDirectory = "";
+			if( File.Exists( textBoxTargetFile.Text ) ) {
+				initialDirectory = Path.GetDirectoryName( textBoxTargetFile.Text );
+			} else if( Directory.Exists( textBoxTargetFile.Text ) ) {
+				initialDirectory = textBoxTargetFile.Text;
+			}
+
+			var f = new OpenFileDialog() {
+				Title = "変換するPDFファイルを選択してください。",
+				InitialDirectory = initialDirectory,
+				Filter = "PDFファイル|*.pdf|すべてのファイル|*.*",
+			};
+			if( f.ShowDialog() != true ) {
+				return;
+			}
+
+			textBoxTargetFile.Text = f.FileName;
+
+			await PDF変換実行();
+		}
+
+		async Task PDF変換実行() {
+			if( !File.Exists( textBoxTargetFile?.Text ) ) {
+				return;
+			}
+
+			button保存.IsEnabled = false;
+
+			viewer.Children.Clear();
+
+			var file = await Windows.Storage.StorageFile.GetFileFromPathAsync( textBoxTargetFile.Text );
+
+			if( IsDPIエラー( comboBoxDPI.Text, out var dpi ) ) {
+				return;
+			}
+
+			var pdfDocument = await PdfDocument.LoadFromFileAsync( file );
+
+			try {
+				progressBar.Visibility = Visibility.Visible;
+				progressBar.Maximum = pdfDocument.PageCount;
+				progressBar.Value = 0;
+
+				for( uint i = 0; i < pdfDocument.PageCount; i++ ) {
+					progressBar.Value++;
+					await Task.Delay( 1 );
+
+					using( var page = pdfDocument.GetPage( i ) ) {
+						using( var stream = new InMemoryRandomAccessStream() ) {
+							var renderOptions = new PdfPageRenderOptions() {
+								DestinationWidth = (uint)Math.Round( page.Dimensions.ArtBox.Width / 96.0 * dpi ),
+								BitmapEncoderId = Windows.Graphics.Imaging.BitmapEncoder.PngEncoderId,
+							};
+							if( RadioButtonJpeg.IsChecked == true ) {
+								renderOptions.BitmapEncoderId = Windows.Graphics.Imaging.BitmapEncoder.JpegEncoderId;
+							}
+
+							await page.RenderToStreamAsync( stream, renderOptions );
+
+							var bitmapFrame = BitmapDecoder.Create( stream.AsStream(), BitmapCreateOptions.None, BitmapCacheOption.OnLoad ).Frames[0];
+
+							viewer.Children.Add(
+								new Border() {
+									Margin = new Thickness( 3 ),
+									BorderThickness = new Thickness( 1 ),
+									BorderBrush = Brushes.Black,
+									Child = new Image() { Source = bitmapFrame }
+								}
+							);
+
+							//using( var fileStream1 = new FileStream( $"./file2{i}.png", FileMode.Create ) ) {
+							//	await RandomAccessStream.CopyAndCloseAsync( stream.GetInputStreamAt( 0 ), fileStream1.AsOutputStream() );
+							//}
+
+						}
+					}
+				}
+
+			} finally {
+				progressBar.Visibility = Visibility.Hidden;
+			}
+
+			button保存.IsEnabled = true;
+		}
+
+		/// <summary>
+		/// 保存
+		/// </summary>
+		private async void Button_Click_1( object sender, RoutedEventArgs e ) {
+
+			var extension = ( RadioButtonPng.IsChecked == true ) ? ".png" : ".jpg";
+			var baseFilename = Path.GetFileNameWithoutExtension( textBoxTargetFile.Text );
+
+			var f = new SaveFileDialog() {
+				Title = "保存フォルダを選択してください。",
+				FileName = baseFilename + extension,
+
+				Filter = ( RadioButtonPng.IsChecked == true ) ? "PNGファイル|*.png" : "JPEGファイル|*.jpg",
+			};
+			if( f.ShowDialog() != true ) {
+				return;
+			}
+
+			baseFilename = Path.GetFileNameWithoutExtension( f.FileName );
+			var saveDir = Path.GetDirectoryName( f.FileName );
+
+			var filenames = Enumerable.Range( 1, viewer.Children.Count ).Select( x => baseFilename + $" ({x})" + extension ).ToArray();
+
+			if( Directory.EnumerateFiles( saveDir ).Any( x => filenames.Contains( Path.GetFileName( x ) ) ) ) {
+				var r = MessageBox.Show( this, "すでにファイルが存在しています。\n上書きしてもよろしいですか？", "上書き確認", MessageBoxButton.OKCancel, MessageBoxImage.Question );
+				if( r != MessageBoxResult.OK ) {
+					return;
+				}
+			}
+
+			try {
+				progressBar.Visibility = Visibility.Visible;
+				progressBar.Maximum = filenames.Length;
+				progressBar.Value = 0;
+
+				var images = VisualTreeHelpers.FindVisualChildren<Image>( viewer ).Select( x => x.Source );
+				foreach( var (image, filename) in images.Zip( filenames, ( a, b ) => (a, b) ) ) {
+					progressBar.Value++;
+					await Task.Delay( 1 );
+
+					BitmapEncoder encoder = null;
+					if( RadioButtonPng.IsChecked == true ) {
+						encoder = new PngBitmapEncoder() {
+							Frames = { BitmapFrame.Create( (BitmapSource)image ) },
+						};
+					} else {
+						encoder = new JpegBitmapEncoder() {
+							Frames = { BitmapFrame.Create( (BitmapSource)image ) },
+						};
+					}
+
+					using( var file = File.Create( Path.Combine( saveDir, filename ) ) ) {
+						encoder.Save( file );
+					}
+				}
+
+				progressBar.Maximum = progressBar.Value;
+				await Task.Delay( 1 );
+
+			} catch( Exception ex ) {
+				MessageBox.Show( this, ex.Message, "エラー", MessageBoxButton.OK, MessageBoxImage.Error );
+
+			} finally {
+				progressBar.Visibility = Visibility.Hidden;
+			}
+		}
+
+		private async void RadioButton_Checked( object sender, RoutedEventArgs e ) {
+			await PDF変換実行();
+		}
+
+
+
+		void AssociatedObject_DragOver( object sender, DragEventArgs e ) {
+			var files = e.Data.GetData( DataFormats.FileDrop ) as string[];
+
+			if( files.Any( x => x.ToLower().EndsWith( ".pdf" ) ) ) {
+				e.Effects = DragDropEffects.All;
+			} else {
+				e.Effects = DragDropEffects.None;
+			}
+
+			e.Handled = true;
+		}
+
+		async void AssociatedObject_Drop( object sender, DragEventArgs e ) {
+			e.Handled = true;
+
+			string[] files = e.Data.GetData( DataFormats.FileDrop ) as string[];
+
+			await ドロップ( files );
+		}
+
+		async Task ドロップ( string[] files ) {
+			var file = files.FirstOrDefault( x => x.ToLower().EndsWith( ".pdf" ) );
+			if( file == null ) {
+				return;
+			}
+
+			textBoxTargetFile.Text = file;
+
+			await PDF変換実行();
+		}
+
+
+		string comboBoxOldValue;
+
+		private async void ComboBoxDPI_SelectionChanged( object sender, SelectionChangedEventArgs e ) {
+
+			if( e.AddedItems.Count < 1 ) {
+				return;
+			}
+			var newValue = (string)e.AddedItems[0];
+
+			if( comboBoxOldValue == newValue ) {
+				return;
+			}
+
+			if( IsDPIエラー( newValue, out var dpi ) ) {
+				return;
+			}
+
+			comboBoxOldValue = newValue;
+
+			await PDF変換実行();
+		}
+
+		private async void ComboBoxDPI_LostFocus( object sender, RoutedEventArgs e ) {
+
+			if( comboBoxOldValue == comboBoxDPI.Text ) {
+				return;
+			}
+
+			if( IsDPIエラー( comboBoxDPI.Text, out var dpi ) ) {
+				return;
+			}
+
+			comboBoxOldValue = comboBoxDPI.Text;
+
+			await PDF変換実行();
+		}
+
+		bool IsDPIエラー( string value, out double dpi ) {
+			if( !double.TryParse( value, out dpi ) ) {
+				MessageBox.Show( this, "解像度には数字を入力してください。", "エラー", MessageBoxButton.OK, MessageBoxImage.Error );
+
+				return true;
+			}
+			if( double.IsNaN( dpi ) || dpi < 10 || 2000 < dpi ) {
+				MessageBox.Show( this, "解像度は10から2000までの値を入力してください。", "エラー", MessageBoxButton.OK, MessageBoxImage.Error );
+
+				return true;
+			}
+
+			return false;
+		}
+
+	}
+}
---
+++ Source/PDFtoImage/VisualTreeHelpers.cs
@@ -0,0 +1,469 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace Libraries {
+	public class VisualTreeHelpers {
+		/// <summary>
+		/// 子要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="current"></param>
+		/// <returns></returns>
+		public static T FindVisualChild<T>( DependencyObject current ) where T : DependencyObject {
+			if( current != null ) {
+				int numVisuals = VisualTreeHelper.GetChildrenCount( current );
+				for( int i = 0; i < numVisuals; i++ ) {
+					var child = VisualTreeHelper.GetChild( current, i );
+					var result = ( child as T ) ?? FindVisualChild<T>( (Visual)child );
+
+					if( result != null ) {
+						return result;
+					}
+				}
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// 子要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="current"></param>
+		/// <returns></returns>
+		public static T FindVisualChild<T>( DependencyObject current, Func<T, bool> predicate ) where T : DependencyObject {
+			if( predicate == null ) {
+				throw new ArgumentException( "条件を指定してください。" );
+			}
+
+			if( current != null ) {
+				int numVisuals = VisualTreeHelper.GetChildrenCount( current );
+				for( int i = 0; i < numVisuals; i++ ) {
+					var child = VisualTreeHelper.GetChild( current, i );
+
+					if( child is T result ) {
+						if( predicate( result ) ) {
+							return result;
+						}
+					}
+
+					result = FindVisualChild<T>( (Visual)child, predicate );
+					if( result != null ) {
+						return result;
+					}
+				}
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// 全ての子要素を取得します。
+		/// </summary>
+		public static IEnumerable<T> FindVisualChildren<T>( DependencyObject current ) where T : DependencyObject {
+			return FindVisualChildren<T>( current, x => true );
+		}
+
+		/// <summary>
+		/// 全ての子要素を取得します。
+		/// </summary>
+		public static IEnumerable<T> FindVisualChildren<T>( DependencyObject current, Func<T, bool> predicate ) where T : DependencyObject {
+			if( predicate == null ) {
+				throw new ArgumentException( "条件を指定してください。" );
+			}
+
+			if( current != null ) {
+				int numVisuals = VisualTreeHelper.GetChildrenCount( current );
+				for( int i = 0; i < numVisuals; i++ ) {
+					var child = VisualTreeHelper.GetChild( current, i );
+
+					{
+						if( child is T result ) {
+							if( predicate( result ) ) {
+								yield return result;
+							}
+						}
+					}
+
+					{
+						var children = FindVisualChildren<T>( (Visual)child, predicate );
+						if( children != null ) {
+							foreach( var result in children ) {
+								yield return result;
+							}
+							continue;
+						}
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// 子要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="current"></param>
+		/// <param name="name">子要素の名前</param>
+		/// <returns></returns>
+		public static T FindVisualChildName<T>( DependencyObject current, string name ) where T : FrameworkElement {
+			if( current != null ) {
+				int numVisuals = VisualTreeHelper.GetChildrenCount( current );
+				for( int i = 0; i < numVisuals; i++ ) {
+					var child = VisualTreeHelper.GetChild( current, i );
+
+					if( child is T result && result.Name == name ) {
+						return result;
+					}
+
+					var _result = FindVisualChildName<T>( child, name );
+					if( _result != null ) {
+						return _result;
+					}
+
+				}
+			}
+
+			return null;
+		}
+
+
+		/// <summary>
+		/// 親要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="current"></param>
+		/// <returns></returns>
+		public static T FindVisualParent<T>( DependencyObject current ) where T : DependencyObject {
+			if( current != null ) {
+				if( current is Visual || current is Visual3D ) {
+					var parent = VisualTreeHelper.GetParent( current );
+
+					return ( parent as T ) ?? FindVisualParent<T>( parent );
+				}
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// 親要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="current"></param>
+		/// <returns></returns>
+		public static IEnumerable<T> EnumerableParents<T>( DependencyObject current ) where T : DependencyObject {
+			while( current != null ) {
+				if( current is Visual || current is Visual3D ) {
+					var parent = VisualTreeHelper.GetParent( current );
+					yield return ( parent as T );
+
+					current = parent;
+				}
+			}
+		}
+
+		/// <summary>
+		/// 親要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="current"></param>
+		/// <returns></returns>
+		public static IEnumerable<DependencyObject> EnumerableParents( DependencyObject current ) {
+			while( current != null ) {
+				if( current is Visual || current is Visual3D ) {
+					var parent = VisualTreeHelper.GetParent( current );
+					yield return parent;
+
+					current = parent;
+				}
+			}
+		}
+
+		/// <summary>
+		/// 親要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="current"></param>
+		/// <returns></returns>
+		public static T FindVisualMostParent<T>( DependencyObject current ) where T : DependencyObject {
+			if( current != null ) {
+				var parent = VisualTreeHelper.GetParent( current );
+
+				var result2 = FindVisualParent<T>( parent );
+
+				if( parent is T result && result2 == null ) {
+					return result;
+				}
+				return result2;
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// 最上位の親要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="current"></param>
+		/// <returns></returns>
+		public static DependencyObject FindVisualMostParent( DependencyObject current ) {
+			if( current != null ) {
+				var parent = VisualTreeHelper.GetParent( current );
+
+				if( parent == null ) {
+					return current;
+				} else {
+					return FindVisualMostParent( parent );
+				}
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// 親要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="current"></param>
+		/// <param name="func">取得条件</param>
+		/// <returns></returns>
+		public static T FindVisualParent<T>( DependencyObject current, Func<T, bool> predicate ) where T : DependencyObject {
+			if( predicate == null ) {
+				throw new ArgumentException( "条件を指定してください。" );
+			}
+
+			if( current != null ) {
+				var result = FindVisualParent<T>( current );
+
+				if( result != null && predicate( result ) ) {
+					return result;
+				} else {
+					return FindVisualParent<T>( result, predicate );
+				}
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// 兄弟要素を全て取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="current"></param>
+		/// <returns></returns>
+		public static IEnumerable<T> FindVisualSiblings<T>( DependencyObject current ) where T : DependencyObject {
+			if( current != null ) {
+				var parent = VisualTreeHelper.GetParent( current );
+				if( parent == null ) {
+					return null;
+				}
+
+				int numVisuals = VisualTreeHelper.GetChildrenCount( parent );
+				return _FindVisualSiblings();
+				IEnumerable<T> _FindVisualSiblings() {
+					for( int i = 0; i < numVisuals; i++ ) {
+						if( VisualTreeHelper.GetChild( parent, i ) is T result ) {
+							yield return result;
+						}
+					}
+				}
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// 兄弟要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="current"></param>
+		/// <returns></returns>
+		public static T FindVisualSibling<T>( DependencyObject current ) where T : DependencyObject {
+			return FindVisualSiblings<T>( current )?.FirstOrDefault();
+		}
+
+		/// <summary>
+		/// ヒットテストを使用して最初の要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="reference"></param>
+		/// <param name="point"></param>
+		/// <returns></returns>
+		public static T HitTestFirstOrDefault<T>( Visual reference, Point point ) where T : DependencyObject => HitTestFirstOrDefault<T>( reference, point, null );
+
+		/// <summary>
+		/// ヒットテストを使用して最初に条件に一致した要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="reference"></param>
+		/// <param name="point"></param>
+		/// <param name="predicate"></param>
+		/// <returns></returns>
+		public static T HitTestFirstOrDefault<T>( Visual reference, Point point, Func<T, bool> predicate ) where T : DependencyObject {
+			return new HitTestHelper<T>().FirstOrDefault( reference, point, predicate );
+		}
+
+		/// <summary>
+		/// ヒットテストを使用してすべての要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="reference"></param>
+		/// <param name="geometry"></param>
+		/// <returns></returns>
+		public static ReadOnlyCollection<T> HitTestAll<T>( Visual reference, Geometry geometry ) where T : DependencyObject => HitTestAll<T>( reference, geometry, null );
+
+		/// <summary>
+		/// ヒットテストを使用して条件に一致したすべての要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="reference"></param>
+		/// <param name="geometry"></param>
+		/// <param name="predicate">条件</param>
+		/// <returns></returns>
+		public static ReadOnlyCollection<T> HitTestAll<T>( Visual reference, Geometry geometry, Func<T, bool> predicate ) where T : DependencyObject {
+			return new HitTestHelper<T>().All( reference, geometry, predicate );
+		}
+
+		/// <summary>
+		/// ヒットテストを使用してすべての要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="reference"></param>
+		/// <param name="point"></param>
+		/// <returns></returns>
+		public static ReadOnlyCollection<T> HitTestAll<T>( Visual reference, Point point ) where T : DependencyObject => HitTestAll<T>( reference, point, null );
+
+		/// <summary>
+		/// ヒットテストを使用して条件に一致したすべての要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="reference"></param>
+		/// <param name="point"></param>
+		/// <param name="predicate">条件</param>
+		/// <returns></returns>
+		public static ReadOnlyCollection<T> HitTestAll<T>( Visual reference, Point point, Func<T, bool> predicate ) where T : DependencyObject {
+			return new HitTestHelper<T>().All( reference, point, predicate );
+		}
+
+		/// <summary>
+		/// ヒットテストを使用して最初の要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="reference"></param>
+		/// <param name="geometry"></param>
+		/// <returns></returns>
+		public static T HitTestFirstOrDefault<T>( Visual reference, Geometry geometry ) where T : DependencyObject => HitTestFirstOrDefault<T>( reference, geometry, null );
+
+		/// <summary>
+		/// ヒットテストを使用して最初に条件に一致した要素を取得します。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="reference"></param>
+		/// <param name="geometry"></param>
+		/// <param name="predicate">条件</param>
+		/// <returns></returns>
+		public static T HitTestFirstOrDefault<T>( Visual reference, Geometry geometry, Func<T, bool> predicate ) where T : DependencyObject {
+			return new HitTestHelper<T>().FirstOrDefault( reference, geometry, predicate );
+		}
+
+
+
+		private class HitTestHelper<T> where T : DependencyObject {
+
+			public T FirstOrDefault( Visual reference, Point point, Func<T, bool> predicate = null ) {
+				this.predicate = predicate;
+
+				VisualTreeHelper.HitTest( reference, null, HitTestFirst, new PointHitTestParameters( point ) );
+
+				return this.Result;
+			}
+
+			public T FirstOrDefault( Visual reference, Geometry geometry, Func<T, bool> predicate = null ) {
+				this.predicate = predicate;
+				VisualTreeHelper.HitTest( reference, null, HitTestFirst, new GeometryHitTestParameters( geometry ) );
+
+				return this.Result;
+			}
+
+			public ReadOnlyCollection<T> All( Visual reference, Point point, Func<T, bool> predicate = null ) {
+				this.predicate = predicate;
+				this.ResultList = new List<T>();
+				VisualTreeHelper.HitTest( reference, null, HitTestAll, new PointHitTestParameters( point ) );
+
+				return this.ResultList.AsReadOnly();
+			}
+
+			public ReadOnlyCollection<T> All( Visual reference, Geometry geometry, Func<T, bool> predicate = null ) {
+				this.predicate = predicate;
+				this.ResultList = new List<T>();
+				VisualTreeHelper.HitTest( reference, null, HitTestAll, new GeometryHitTestParameters( geometry ) );
+
+				return this.ResultList.AsReadOnly();
+			}
+
+			private T Result;
+			private List<T> ResultList;
+
+			Func<T, bool> predicate;
+
+			private HitTestResultBehavior HitTestFirst( HitTestResult result ) {
+				this.Result = result.VisualHit as T;
+				if( this.Result == null ) {
+					this.Result = FindVisualParent<T>( result.VisualHit );
+				}
+
+				if( this.Result != null ) {
+					if( predicate != null ) {
+						if( predicate.Invoke( this.Result ) ) {
+							return HitTestResultBehavior.Stop;
+						} else {
+							this.Result = null;
+							return HitTestResultBehavior.Continue;
+						}
+					}
+
+					return HitTestResultBehavior.Stop;
+				}
+
+				return HitTestResultBehavior.Continue;
+			}
+
+			private HitTestResultBehavior HitTestAll( HitTestResult result ) {
+				T hitElement = null;
+
+				if( result.VisualHit is T t ) {
+					hitElement = t;
+				} else {
+					var parent = VisualTreeHelpers.FindVisualParent<T>( result.VisualHit );
+					if( parent != null ) {
+						hitElement = parent;
+					}
+				}
+
+				if( hitElement != null && this.Result != hitElement ) {
+					if( predicate != null ) {
+						if( predicate.Invoke( this.Result ) ) {
+							ResultList.Add( hitElement );
+						}
+					} else {
+						ResultList.Add( hitElement );
+					}
+
+					this.Result = hitElement;
+				}
+
+				return HitTestResultBehavior.Continue;
+			}
+		}
+
+
+
+	}
+}
